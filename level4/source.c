da67f85583a3340d93f0e71c29851aa0436e18e0
Guilhem DE LA BROSSE <gdelabro@e1r2p18.42.fr>
2019-11-13 17:35:17 +0100

    level4

---
+++ level4/source.c
@@ -0,0 +1,34 @@
+int		main(void)
+{
+	int		pid = fork();                                       // esp+0xac
+	char	str[32];                                            // esp+0x20
+
+	memset(str, '\0', 32);
+	int		ptrace_ret = 0;                                     // esp+0xa8
+	int		stat_loc = 0;                                       // esp+0x1c
+	if (pid == 0)
+	{
+		prctl(1, 1);
+		ptrace(0, 0, NULL, NULL);                               // PTRACE_TRACEME
+		puts("Give me some shellcode, k");
+		gets(str);
+	}
+	else
+	{
+		while (true) {
+			wait(&stat_loc);
+			if ((stat_loc & 0x7f) != 0) {
+				if (WIFEXITED(stat_loc) > 0) {
+					puts("child is exiting...");
+					return (0);
+				}
+				ptrace_ret = ptrace(3, pid, 44, NULL)           // PTRACE_PEEKUSER
+					if (ptrace_ret == 11)                           // exec() syscall
+						break ;
+			}
+		}
+		puts("no exec() for you");
+		kill(pid, SIGKILL);
+	}
+	return (0);
+}
